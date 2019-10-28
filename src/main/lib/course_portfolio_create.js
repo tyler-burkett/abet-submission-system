68a8a833b13c8db788568fe48b10907dc5b34005
Tyler Brian Burkett <tyler.burkett@xela.cc>
2019-10-27 22:32:08 -0400

    Fixed minor issues; Added test cases and code for creating PDFs

--- src/main/lib/course_portfolio_create.js
+++ src/main/lib/course_portfolio_create.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const PDFDocument = require('pdfkit');
+var fs = require('fs');
 
 function summarize(course_portfolio) {
     //TODO
@@ -9,12 +10,159 @@ function summarize(course_portfolio) {
 
 function create_course_summary_PDF(course_summary) {
 
-    //TODO
+    /*
+    expect object with the following members:
+    course_name
+    course_number
+    instructor_name
+    section
+    semester
+    year
+    number_students
+    */
+    var members = ['course_name',
+        'course_number',
+        'instructor_name',
+        'section',
+        'semester',
+        'year',
+        'number_students',
+        'course_score',
+        'slo_scores',
+        'artifact_scores',
+    ];
+
+    // Verify object has all the needed properties
+    // and propertie values are valid
+    for (var name in members) {
+        if (course_summary.hasOwnProperty(name)) {
+            if (course_summary[name] == null || course_summary[name] == undefined) {
+                throw Error('Invalid value for property' + name);
+            }
+        }
+        else {
+            throw Error('Neccessary property' + name + 'missing');
+        }
+    }
+
+    // Create PDF
+    const doc = new PDFDocument;
+    doc.pipe(fs.createWriteStream('tmp/course_portfolio/course_summary.pdf'));
+
+    // Create Title
+    doc.font('fonts/PalatinoBold.ttf')
+        .fontSize(25)
+        .text('Course Summary');
+
+    // Fill in PDF with data from course summary
+    for (var name in members) {
+        doc.font('fonts/PalatinoBold.ttf')
+        .fontSize(12)
+        .text(name + ': ' + String(course_summary[name]));
+    }
+
+    // close the PDF document to save changes
+    doc.end();
+
+    // PDF is made at this point, so indicate success
     return true;
 }
 
-function create_SLO_PDFs(artifacts) {
+function create_SLO_PDFs(artifacts_and_evals) {
 
-    //TODO
+    /* 
+    expect array of SLO artifact objects with the following members:
+    artifact_index
+    artifact_name
+    is_group_assignment
+    number_of_submissions
+    evaluations (array)
+        eval_index
+        student_index
+        evaluation
+        file
+    */
+
+    var artifact_members = [
+        'slo_index',
+        'slo_description',
+        'artifact_index',
+        'artifact_name',
+        'is_group_assignment',
+        'number_of_submissions',
+        'evaluations'
+    ];
+    var evaluation_members = [
+        'eval_index',
+        'student_index',
+        'evaluation',
+        'file'
+    ];
+
+    // Verify object has all the needed properties
+    // and propertie values are valid
+    for (var object in artifacts_and_evals)
+        for (var name in artifact_members) {
+            if (object.hasOwnProperty(name)) {
+                if (course_summary[name] == null || course_summary[name] == undefined) {
+                    throw Error('Invalid value for property' + name);
+                }
+            }
+            else {
+                throw Error('Neccessary property' + name + 'missing');   
+            }
+        }
+        if(eval.length != object.number_of_submissions) {
+            throw Error('Mismatch between number of evaluations and number indicated');
+        }
+        for(var eval in object.evaluations) {
+            for (var name in evaluation_members) {
+                if (!(eval.hasOwnProperty(name))) {
+                    if (course_summary[name] == null || course_summary[name] == undefined) {
+                        throw Error('Invalid value for property' + name + 'in an evaluation');
+                    }
+                }
+                else {
+                    throw Error('Neccessary property' + name + 'missing in an evaluation');   
+                }
+            }
+        }
+
+    for (var artifact in artifacts_and_evals) {
+        for(var eval in artifact.evaluations) {
+            // Create PDF
+            const doc = new PDFDocument;
+            doc.pipe(fs.createWriteStream('tmp/course_portfolio/slo_' + String(artifact.slo_index) + 
+                                        '/artifact_' + String(artifact.artifact_index) + 
+                                        '/student_eval_' + String(eval.eval_index) + '_' + String(eval.student_index) + '.pdf'));
+
+            // Create Title
+            doc.font('fonts/PalatinoBold.ttf')
+                .fontSize(25)
+                .text('Student Evaluation');
+
+            // Fill in PDF with data from artifact
+            for (var name in artifact_members.slice(0,4)) {
+                doc.font('fonts/PalatinoBold.ttf')
+                .fontSize(12)
+                .text(name + ': ' + String(artifact[name]));
+            }
+
+            // Fill in PDF with data from evaluation
+            for (var name in evaluation_members.slice(0,3)) {
+                doc.font('fonts/PalatinoBold.ttf')
+                .fontSize(12)
+                .text(name + ': ' + String(eval[name]));
+            }
+
+            //TODO: how to attach pdf? how is pdf specified via object (path, raw data) ?
+
+            // close the PDF document to save changes
+            doc.end();
+        }
+    }
+
+    // PDF is made at this point, so indicate success
     return true;
 }
+    module.exports = {summarize, create_course_summary_PDF, create_SLO_PDFs};
--- src/test/cases/User_Story_5/test.js
+++ src/test/cases/User_Story_5/test.js
@@ -1,11 +1,12 @@
 //const course_portfolio = require('../../../main/lib/course_portfolio')
+var create = require('../../../main/lib/course_portfolio_create')
 const { expect } = require('../../chai')
 const sinon = require('sinon')
 
 // we use a sandbox so that we can easily restore all stubs created in that sandbox
 const sandbox = sinon.createSandbox();
 
-describe('User Story 3 - Download Course Portfolio', () => {
+describe('User Story 5 - Download Course Portfolio', () => {
 
     describe('summarize', () => {
 
@@ -17,7 +18,11 @@ describe('User Story 3 - Download Course Portfolio', () => {
         })
         
         it('summarizes course', async () => {
+            //Arrange
 
+            //Act
+
+            //Assert
 
         })
 
@@ -31,8 +36,36 @@ describe('User Story 3 - Download Course Portfolio', () => {
 			sandbox.restore()
         })
 
-        it('creates PDF', () => {
+        it('creates course summary PDF', () => {
+            //Arrange
+            var input = {
+                'course_name': '',
+                'course_number': 'CS498',
+                'instructor_name': 'Ethan Toney',
+                'section': 1,
+                'semester': 'Fall',
+                'year': 2019,
+                'number_students' : 5,
+                'course_score': 0.88,
+                'slo_scores': [0.88],
+                'artifact_scores': [0.88, 0.88, 0.88],
+                //'evaluation_scores': []  //what does this mean?
+            };
+            //Act
+            //Assert
+            expect(() => {return create.create_course_summary_PDF(input)}).to.be.true;
 
+            //TODO: validate PDF; manually or automatically?
+        })
+
+        it('throws error on invalid input', () => {
+            var input = {
+            };
+            //Act
+            //Assert
+            expect(() => {return create.create_course_summary_PDF(input)}).to.throw();
+
+            //TODO: validate no PDF; manually or automatically?
         })
 
     })
@@ -46,7 +79,51 @@ describe('User Story 3 - Download Course Portfolio', () => {
         })
 
         it('creates PDFs', () => {
+            //Arrange
+            var input = [{
+                'slo_index': 1,
+                'slo_description': 'SLO 1',
+                'artifact_index': 1,
+                'artifact_name': "artifact 1",
+                'is_group_assignment': False,
+                'number_of_submissions': 1,
+                'evaluations': [{
+                    'eval_index': 1,
+                    'student_index': 1,
+                    'evaluation': [],
+                    'file': null
+                }]
+            }, 
+            {
+                'slo_index': 2,
+                'slo_description': 'SLO 2',
+                'artifact_index': 2,
+                'artifact_name': "artifact 2",
+                'is_group_assignment': False,
+                'number_of_submissions': 1,
+                'evaluations': [{
+                    'eval_index': 1,
+                    'student_index': 1,
+                    'evaluation': [],
+                    'file': null
+                }]
+            }];
 
+            //Act
+            //Assert
+            expect(() => {return create.create_SLO_PDFs(input)}).to.be.true;
+            //TODO: validate PDFs; manually or automatically?
+        })
+
+        it('throws error on invalid input', () => {
+            //Arrange
+            var input = {
+            };
+            //Act
+            //Assert
+            expect(() => {return create.create_course_summary_PDF(input)}).to.throw();
+
+            //TODO: validate no PDF; manually or automatically?
         })
 
     })
