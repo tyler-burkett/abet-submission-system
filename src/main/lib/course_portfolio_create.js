3cf6b99084bf891c8c4c109682e346fd9c560784
Tyler Brian Burkett <tyler.burkett@xela.cc>
2019-10-29 23:02:09 -0400

    Modified test cases and code to reflect recent realizations in data input

--- src/main/lib/course_portfolio_create.js
+++ src/main/lib/course_portfolio_create.js
@@ -4,21 +4,95 @@ const PDFDocument = require('pdfkit');
 var fs = require('fs');
 
 function summarize(course_portfolio) {
-    //TODO
-    return {};
+    
+    var summary = {};
+
+    // get name (null if name not present)
+    if(!course_portfolio.course.hasOwnProperty('course_name'))
+        summary['course_name'] = null;
+    else
+        summary['course_name'] = course_portfolio.course.course_name;
+    
+    // get course number (department abbreviation + course number)
+    summary['course_number'] = course_portfolio.course.department.identifier.toUpperCase() + 
+                               String(course_portfolio.course.number);
+
+    // get intsructor name (or link blue id if not present)
+    if (!course_portfolio.instrcutor.hasOwnProperty('instructor_name'))
+        summary['instructor_name'] = course_portfolio.instrcutor.linkblue_username;
+    else 
+        summary['instructor_name'] = course_portfolio.instrcutor.instructor_name;
+    
+    // get section
+    summary['section'] = course_portfolio.section;
+
+    // get semester
+    summary['semester'] = course_portfolio.semester.value;
+
+    // get year
+    summary['year'] = course_portfolio.year;
+
+    //get number of students 
+    summary['num_students'] = course_portfolio.num_students;
+
+    //Calculate scores
+
+    //Artifact scores (% meet or exceed for an artifact)
+    var artifact_scores = {};
+    for (var outcome in course_portfolio.outcomes) {
+        for (var artifact in outcome.artifacts) {
+            if (artifact_scores.hasOwnProperty(String(artifact.id)))
+                continue;
+            var num_exceed_meet = 0;
+            var total = 0;
+            for (var eval in artifact.evaluations) {
+                if (eval.evaluation.value == 'meets' || eval.evaluation.value == 'exceeds')
+                    num_exceed_meet += 1;
+                total++;
+            }
+            artifact_scores[String(artifact.id)] = { 'score' : num_exceed_meet / total, 
+                                                     'portfolio_slo_id' : artifact.portfolio_slo_id };
+        }
+    }
+
+    summary['artifact_scores'] = artifact_scores;
+
+    //SLO scores (average of artifacts)
+    var slo_scores = [];
+    for (var outcome in course_portfolio.outcomes) {
+        var sum = 0;
+        var total = 0;
+        var current_slo_id = outcome.slo_id;
+        for (var artifact_id in artifact_scores) {
+            if (artifact_scores[artifact_id].portfolio_slo_id == current_slo_id) {
+                sum += artifact_scores[artifact_id].score;
+                total++;
+            }
+        }
+        var avg = sum / total;
+        slo_scores.push({'slo_description': outcome.slo.description, 'slo_score': avg});
+    }
+
+    summary['slo_scores'] = slo_scores;
+
+    //course score (average of SLO scores)
+    var sum = 0;
+    var total = 0;
+    for (var slo in slo_scores) {
+        sum += slo.slo_score;
+        total++;
+    }
+    summary['course_score'] = sum / total;
+
+    summary['student_evals'] = course_portfolio.outcomes;
+
+    return summary;
 } 
 
-function create_course_summary_PDF(course_summary) {
+function create_course_summary_PDF(course_summary, directory) {
 
     /*
     expect object with the following members:
-    course_name
-    course_number
-    instructor_name
-    section
-    semester
-    year
-    number_students
     */
     var members = ['course_name',
         'course_number',
@@ -26,14 +100,16 @@ function create_course_summary_PDF(course_summary) {
         'section',
         'semester',
         'year',
-        'number_students',
+        'num_students',
         'course_score',
         'slo_scores',
         'artifact_scores',
+        'student_evals'
     ];
 
     // Verify object has all the needed properties
     // and propertie values are valid
+    /*
     for (var name in members) {
         if (course_summary.hasOwnProperty(name)) {
             if (course_summary[name] == null || course_summary[name] == undefined) {
@@ -44,10 +120,11 @@ function create_course_summary_PDF(course_summary) {
             throw Error('Neccessary property' + name + 'missing');
         }
     }
+    */
 
     // Create PDF
     const doc = new PDFDocument;
-    doc.pipe(fs.createWriteStream('tmp/course_portfolio/course_summary.pdf'));
+    doc.pipe(fs.createWriteStream(directory + '/course_summary.pdf'));
 
     // Create Title
     doc.font('fonts/PalatinoBold.ttf')
@@ -68,7 +145,7 @@ function create_course_summary_PDF(course_summary) {
     return true;
 }
 
-function create_SLO_PDFs(artifacts_and_evals) {
+function create_SLO_PDFs(artifacts_and_evals, directory) {
 
     /* 
     expect array of SLO artifact objects with the following members:
@@ -101,6 +178,7 @@ function create_SLO_PDFs(artifacts_and_evals) {
 
     // Verify object has all the needed properties
     // and propertie values are valid
+    /*
     for (var object in artifacts_and_evals)
         for (var name in artifact_members) {
             if (object.hasOwnProperty(name)) {
@@ -127,12 +205,13 @@ function create_SLO_PDFs(artifacts_and_evals) {
                 }
             }
         }
+        */
 
     for (var artifact in artifacts_and_evals) {
         for(var eval in artifact.evaluations) {
             // Create PDF
             const doc = new PDFDocument;
-            doc.pipe(fs.createWriteStream('tmp/course_portfolio/slo_' + String(artifact.slo_index) + 
+            doc.pipe(fs.createWriteStream(directory + '/slo_' + String(artifact.slo_index) + 
                                         '/artifact_' + String(artifact.artifact_index) + 
                                         '/student_eval_' + String(eval.eval_index) + '_' + String(eval.student_index) + '.pdf'));
 
@@ -165,4 +244,6 @@ function create_SLO_PDFs(artifacts_and_evals) {
     // PDF is made at this point, so indicate success
     return true;
 }
+
+//express download function
     module.exports = {summarize, create_course_summary_PDF, create_SLO_PDFs};
--- src/test/cases/User_Story_5/test.js
+++ src/test/cases/User_Story_5/test.js
@@ -6,9 +6,126 @@ const sinon = require('sinon')
 // we use a sandbox so that we can easily restore all stubs created in that sandbox
 const sandbox = sinon.createSandbox();
 
-describe('User Story 5 - Download Course Portfolio', () => {
+// sample input for course portfolio used by multiple tests
+var course_input = {
+    "id": 1,
+    "course_id": 1,
+    "instructor_id": 1,
+    "semester_term_id": 1,
+    "num_students": 5,
+    "section": 1,
+    "year": 2019,
+    "course": {
+        "id": 1,
+        "department_id": 1,
+        "number": 498,
+        "course_name": "Software Engineering for Senior Project",
+        "department": {
+            "id": 1,
+            "identifier": "cs",
+            "name": "Computer Science"
+        }
+    },
+    "instructor": {
+        "id": 1,
+        "instructor_name": "Ethan Toney",
+        "linkblue_username": "ethan.toney"
+    },
+    "semester": {
+        "id": 1,
+        "type": 1,
+        "value": "fall"
+    },
+    "outcomes": [
+        {
+            "id": 1,
+            "portfolio_id": 1,
+            "slo_id": 1,
+            "slo": {
+                "id": 1,
+                "department_id": 1,
+                "index": 2,
+                "description": "Design, implement, and evaluate a computing-based solution to meet a given set of computing requirements in the context of the program's discipline.",
+                "metrics": [
+                    {
+                        "id": 1,
+                        "slo_id": 1,
+                        "index": 1,
+                        "name": "Identify and interpret client needs and design constraints",
+                        "exceeds": "n/a",
+                        "meets": "n/a",
+                        "partially": "n/a",
+                        "not": "n/a"
+                    }
+                ]
+            },
+            "artifacts": [
+                {
+                    "id": 1,
+                    "portfolio_slo_id": 1,
+                    "index": 1,
+                    "name": "_unset_",
+                    "evaluations": [
+                        {
+                            "id": 1,
+                            "artifact_id": 1,
+                            "evaluation_index": 1,
+                            "student_index": 1,
+                            "evaluation": {
+                                "id": 8,
+                                "value" : "meets"
+                            },
+                            "file": null
+                        }
+                    ]
+                },
+                {
+                    "id": 2,
+                    "portfolio_slo_id": 1,
+                    "index": 2,
+                    "name": "_unset_",
+                    "evaluations": [
+                        {
+                            "id": 6,
+                            "artifact_id": 2,
+                            "evaluation_index": 1,
+                            "student_index": 1,
+                            "evaluation": {
+                                "id": 7,
+                                "value" : "exceeds"
+                            },
+                            "file": null
+                        }
+                    ]
+                },
+                {
+                    "id": 3,
+                    "portfolio_slo_id": 1,
+                    "index": 3,
+                    "name": "_unset_",
+                    "evaluations": [
+                        {
+                            "id": 11,
+                            "artifact_id": 3,
+                            "evaluation_index": 1,
+                            "student_index": 1,
+                            "evaluation": {
+                                "id": 9,
+                                "value" : "partially"
+                            },
+                            "file": null
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
 
-    describe('summarize', () => {
+
+describe('Epic 5 - Download Course Portfolio', () => {
+
+    describe('Task 2 - summarize course portfolio (function summarize)', () => {
 
         // this is ran after each unit test
 		afterEach(() => {
@@ -17,18 +134,50 @@ describe('User Story 5 - Download Course Portfolio', () => {
 			sandbox.restore()
         })
         
-        it('summarizes course', async () => {
+        it('summarizes course', () => {
             //Arrange
+            let course_input = {...course_input};
 
             //Act
-
+            var output = create.summarize(course_input);
+            
             //Assert
+            expectoutputto.deep.equal({  
+            });
+
+        })
+
+        it('uses linkblue ID if instructor name not found', () => {
+            //Arrange
+            let course_input = {...course_input};
+            delete course_input.instructor_name;
+
+            //Act
+            var output = create.summarize(course_input);
+            
+            //Assert
+            expectoutputto.deep.equal({  
+            });
+
+        })
+
+        it('leaves null if course name not found', () => {
+            //Arrange
+            let course_input = {...course_input};
+            delete course_input.course_name;
+
+            //Act
+            var output = create.summarize(course_input);
+            
+            //Assert
+            expectoutputto.deep.equal({  
+            });
 
         })
 
     })
 
-    describe('create_course_summary_PDF', () => {
+    describe('Task 3 - create course summary pdf (create_course_summary_PDF)', () => {
         // this is ran after each unit test
 		afterEach(() => {
 			// this is needed to restore the CoursePortfolio model back to it's original state
@@ -39,17 +188,100 @@ describe('User Story 5 - Download Course Portfolio', () => {
         it('creates course summary PDF', () => {
             //Arrange
             var input = {
-                'course_name': '',
+                'course_name': 'Software Engineering for Senior Project',
                 'course_number': 'CS498',
                 'instructor_name': 'Ethan Toney',
                 'section': 1,
-                'semester': 'Fall',
+                'semester': 'fall',
                 'year': 2019,
                 'number_students' : 5,
                 'course_score': 0.88,
                 'slo_scores': [0.88],
-                'artifact_scores': [0.88, 0.88, 0.88],
-                //'evaluation_scores': []  //what does this mean?
+                'artifact_scores': [0.88, 0.90, 0.73],
+                'student_evals': [
+                    {
+                        "id": 1,
+                        "portfolio_id": 1,
+                        "slo_id": 1,
+                        "slo": {
+                            "id": 1,
+                            "department_id": 1,
+                            "index": 2,
+                            "description": "Design, implement, and evaluate a computing-based solution to meet a given set of computing requirements in the context of the program's discipline.",
+                            "metrics": [
+                                {
+                                    "id": 1,
+                                    "slo_id": 1,
+                                    "index": 1,
+                                    "name": "Identify and interpret client needs and design constraints",
+                                    "exceeds": "n/a",
+                                    "meets": "n/a",
+                                    "partially": "n/a",
+                                    "not": "n/a"
+                                }
+                            ]
+                        },
+                        "artifacts": [
+                            {
+                                "id": 1,
+                                "portfolio_slo_id": 1,
+                                "index": 1,
+                                "name": "_unset_",
+                                "evaluations": [
+                                    {
+                                        "id": 1,
+                                        "artifact_id": 1,
+                                        "evaluation_index": 1,
+                                        "student_index": 1,
+                                        "evaluation": {
+                                            "id": 8,
+                                            "value" : "meets"
+                                        },
+                                        "file": null
+                                    }
+                                ]
+                            },
+                            {
+                                "id": 2,
+                                "portfolio_slo_id": 1,
+                                "index": 2,
+                                "name": "_unset_",
+                                "evaluations": [
+                                    {
+                                        "id": 6,
+                                        "artifact_id": 2,
+                                        "evaluation_index": 1,
+                                        "student_index": 1,
+                                        "evaluation": {
+                                            "id": 7,
+                                            "value" : "exceeds"
+                                        },
+                                        "file": null
+                                    }
+                                ]
+                            },
+                            {
+                                "id": 3,
+                                "portfolio_slo_id": 1,
+                                "index": 3,
+                                "name": "_unset_",
+                                "evaluations": [
+                                    {
+                                        "id": 11,
+                                        "artifact_id": 3,
+                                        "evaluation_index": 1,
+                                        "student_index": 1,
+                                        "evaluation": {
+                                            "id": 9,
+                                            "value" : "partially"
+                                        },
+                                        "file": null
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
             };
             //Act
             //Assert
